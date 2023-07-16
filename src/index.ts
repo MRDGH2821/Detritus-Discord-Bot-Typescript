f3d0e9936df31f636f52d200809bcb1625c7c042
MRDGH2821 <mihirrabade@gmail.com>
2023-07-16 16:25:07 +0530

    fix: fix events not being imported

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,30 @@
-import { ClusterClient, InteractionCommandClient } from "detritus-client";
+import { InteractionCommandClient } from "detritus-client";
 import { DISCORD_TOKEN } from "./lib/EnvValues";
-import { readdirSync } from "fs";
+import { resolve } from "path";
+import { readdir } from "fs/promises";
 
-const bot = new InteractionCommandClient(DISCORD_TOKEN, {
-  useClusterClient: true,
-});
-
-// Adding interaction commands
-bot.addMultipleIn("./src/commands");
-
-// Adding Events
-readdirSync("./events").forEach((file) => {
-  const path = process.cwd() + "/events/" + file;
-  import(path).then((event) => {
-    event.default(bot);
+async function main() {
+  const bot = new InteractionCommandClient(DISCORD_TOKEN, {
+    useClusterClient: true,
   });
-});
 
-bot.run();
+  // Adding interaction commands
+  await bot.addMultipleIn("./commands");
+
+  // Adding events
+  const eventsDir = resolve(__dirname, "events");
+
+  await readdir(eventsDir).then((files) =>
+    files.forEach((file) => {
+      const path = resolve(eventsDir, file);
+      import(path).then((event) => {
+        const fn = event.default;
+        bot.client.on(fn.event, fn.run);
+      });
+    })
+  );
+
+  bot.run();
+}
+
+main();
