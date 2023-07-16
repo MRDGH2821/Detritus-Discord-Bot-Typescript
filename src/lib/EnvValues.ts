403245d9b2bd76fc215dcec253da581043d5153b
MRDGH2821 <mihirrabade@gmail.com>
2023-07-16 16:27:26 +0530

    style: format using eslint

--- src/lib/EnvValues.ts
+++ src/lib/EnvValues.ts
@@ -6,4 +6,4 @@ if (!process.env.DISCORD_TOKEN) {
   throw new Error("DISCORD_TOKEN is not defined");
 }
 
-export const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
+export const { DISCORD_TOKEN } = process.env;
