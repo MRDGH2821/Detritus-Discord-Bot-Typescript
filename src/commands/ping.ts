2498f46d3f6708a0a2cb2de8bf789ecd0eddcf35
MRDGH2821 <mihirrabade@gmail.com>
2023-07-16 16:26:15 +0530

    fix: fix string formatting

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -4,6 +4,9 @@ export default new InteractionCommand({
   name: "ping",
   description: "Gives you the ping of the bot",
   async run(ctx) {
-    await ctx.editOrRespond(`Pong! ${await ctx.client.ping()}ms`);
+    const ping = await ctx.client.ping();
+    await ctx.editOrRespond(
+      `Pong! \`\`\`json\n${JSON.stringify(ping, null, 2)} \n\`\`\``
+    );
   },
 });
