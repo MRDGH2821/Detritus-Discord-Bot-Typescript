250f7aac93a305cdcd4c3f55ac59b84b2775ee2e
MRDGH2821 <mihirrabade@gmail.com>
2023-07-17 20:41:32 +0530

    feat: add command client & prefix command

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
-import { InteractionCommandClient } from "detritus-client";
+import {
+  ClusterClient,
+  CommandClient,
+  InteractionCommandClient,
+} from "detritus-client";
 import { DISCORD_TOKEN } from "./lib/EnvValues";
 import { resolve } from "path";
 import { readdir } from "fs/promises";
+import { GatewayIntents } from "detritus-client/lib/constants";
+import { CommandOptions } from "detritus-client/lib/command";
 
 async function main() {
-  const bot = new InteractionCommandClient(DISCORD_TOKEN, {
-    useClusterClient: true,
+  const baseBot = new ClusterClient(DISCORD_TOKEN, {
+    gateway: {
+      intents: [GatewayIntents.DIRECT_MESSAGES, GatewayIntents.GUILD_MESSAGES],
+    },
   });
-
-  // Adding interaction commands
-  await bot.addMultipleIn("./commands");
 
   // Adding events
   const eventsDir = resolve(__dirname, "events");
@@ -17,14 +22,64 @@ async function main() {
   await readdir(eventsDir).then((files) =>
     files.forEach((file) => {
       const path = resolve(eventsDir, file);
-      import(path).then((event) => {
-        const fn = event.default;
-        bot.client.on(fn.event, fn.run);
+      import(path).then(
+        (event: {
+          default: {
+            event: string;
+            run: (...args: unknown[]) => unknown;
+          };
+        }) => {
+          const fn = event.default;
+          baseBot.on(fn.event, fn.run);
+        }
+      );
+    })
+  );
+
+  // Slash Command bot
+  const slashBot = new InteractionCommandClient(baseBot);
+
+  // Add interaction commands
+  await slashBot.addMultipleIn("./commands");
+
+  // Prefix Command bot
+  const prefixBot = new CommandClient(slashBot, {
+    prefix: "d!",
+    prefixes: ["!", "$"],
+    gateway: {
+      intents: [GatewayIntents.DIRECT_MESSAGES, GatewayIntents.GUILD_MESSAGES],
+    },
+    mentionsEnabled: true,
+  });
+
+  // Add prefix commands
+  const prefixCmdDir = resolve(__dirname, "prefixCommands");
+  await readdir(prefixCmdDir).then((files) =>
+    files.forEach((file) => {
+      const path = resolve(prefixCmdDir, file);
+      import(path).then((event: { default: CommandOptions }) => {
+        const cmd = event.default;
+        prefixBot.add(cmd);
       });
     })
   );
 
-  bot.run();
+  prefixBot.add(
+    {
+      name: "ping",
+    },
+    async (ctx) => {
+      const ping = await ctx.client.ping();
+      console.log(ctx.content);
+      await ctx.editOrReply(
+        `Pong! \`\`\`json\n${JSON.stringify(ping, null, 2)} \n\`\`\``
+      );
+    }
+  );
+
+  await prefixBot.run().then(() => console.log("Prefix bot is ready!"));
+  await slashBot.run().then(() => console.log("Slash bot is ready!"));
+  await baseBot.run().then(() => console.log("Base bot is ready!"));
 }
 
 main();
